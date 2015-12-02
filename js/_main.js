6a008af58bc69b9af7b1592a792cebec8f14f4a1
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-12-01 20:54:17 -0500

    finished design

--- js/_main.js
+++ js/_main.js
@@ -1,9 +1,1 @@
-( function( $ ) {
-  // Init Skrollr
-  var s = skrollr.init ({
-    render: function(data) {
-	      //Debugging - Log the current scroll position
-	      //console.log(data.curTop);
-	    }
-  });
-});
+$('.fullscreen').css('height', $(window).height() );
